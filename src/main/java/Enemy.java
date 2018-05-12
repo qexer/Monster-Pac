596f3a6e46094283b7a864da49ab50b98fe97b05
qexer <qexer7@gmail.com>
2018-05-12 19:07:30 +0200

    comments added

--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -1,4 +1,11 @@
-// TODO: #import Position #import Field #import Orientation
+import Position;
+import Field;
+import Orientation;
+
+/**
+ * <h1>Enemy osztály</h1>
+ * A táblán elhelyezkedő ellenségek reprezentálására szolgál.
+ */
 public class Enemy extends Movable{
 
 }
--- src/main/java/Movable.java
+++ src/main/java/Movable.java
@@ -1,10 +1,23 @@
-// TODO: #import Position #import Field  #import Orientation
+import Position;
+import Field;
+import Orientation;
+
+/**
+ * <h1>Movable osztály</h1>
+ * A táblán lévő minden mozgatható objektum alapja. Ebből származik a Player és az Enemy osztály.
+ * <b>Note:</b> Csak az alosztályai léteznek a táblán.
+ */
 // X means the horizontal line on the game table
 
 public class Movable {
     Orientation orientation;
     Position pos;
 
+    /**
+     * Mozgást leíró metódus. A jelenlegi iránynak megfelelően lép egy mezőt a táblán.
+     * @param p Jelenlegi irány
+     * @return
+     */
     Position MoveFrom(Position p){
         switch(orientation){
             case "UP": pos.setY(pos.getY()-1); break;
@@ -14,6 +27,16 @@ public class Movable {
         }
         return pos;
     }
+
+    /**
+     * Mezővel való interakció
+     * @param f Mező
+     */
     void interactWith(Field f){}
+
+    /**
+     * Irányt beállító metódus.
+     * @param o Irány
+     */
     void setOrientation(Orientation o){ orientation = o; }
 }
