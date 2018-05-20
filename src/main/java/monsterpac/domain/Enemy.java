9775a10f82336ccdd8cc7297abd30877baedeb74
barmerc <36930010+barmerc@users.noreply.github.com>
2018-05-20 22:31:01 +0200

    fix Enemy.moveFrom()

--- src/main/java/monsterpac/domain/Enemy.java
+++ src/main/java/monsterpac/domain/Enemy.java
@@ -13,59 +13,59 @@ public class Enemy extends Movable {
 
     /**
      * Mozgást leíró metódus. A jelenlegi iránynak megfelelően lép egy mezőt a táblán.
-     * @param p Jelenlegi irány
-     * @return Hova fog lépni
+     * @param position Jelenlegi pozíció
+     * @return Hova lépett
      */
     @Override
-    Position MoveFrom(Position p){
-        if(lastpos != p) {
+    public Position moveFrom(Position position){
+        if(lastpos != position) {
             switch (orientations) {
                 case UP:
-                    pos.setY(pos.getY() - 1);
+                    position.setY(position.getY() - 1);
                     break;
                 case DOWN:
-                    pos.setY(pos.getY() + 1);
+                    position.setY(position.getY() + 1);
                     break;
                 case LEFT:
-                    pos.setX(pos.getX() - 1);
+                    position.setX(position.getX() - 1);
                     break;
                 case RIGHT:
-                    pos.setX(pos.getX() + 1);
+                    position.setX(position.getX() + 1);
                     break;
             }
         }else{ // Helyben maradtunk az előbb, új írány kell. (Falba ütköztünk vagy hasonló.)
             switch (orientations) {
                 case UP:
                     switch(rand.nextInt(3)) {
-                        case 0: setOrientations(Orientations.DOWN); pos.setY(pos.getY() + 1); break;
-                        case 1: setOrientations(Orientations.LEFT); pos.setX(pos.getX() - 1); break;
-                        case 2: setOrientations(Orientations.RIGHT); pos.setX(pos.getX() + 1); break;
+                        case 0: setOrientations(Orientations.DOWN); position.setY(position.getY() + 1); break;
+                        case 1: setOrientations(Orientations.LEFT); position.setX(position.getX() - 1); break;
+                        case 2: setOrientations(Orientations.RIGHT); position.setX(position.getX() + 1); break;
                     }
                     break;
                 case DOWN:
                     switch(rand.nextInt(3)) {
-                        case 0: setOrientations(Orientations.UP); pos.setY(pos.getY() - 1); break;
-                        case 1: setOrientations(Orientations.LEFT); pos.setX(pos.getX() - 1); break;
-                        case 2: setOrientations(Orientations.RIGHT); pos.setX(pos.getX() + 1); break;
+                        case 0: setOrientations(Orientations.UP); position.setY(position.getY() - 1); break;
+                        case 1: setOrientations(Orientations.LEFT); position.setX(position.getX() - 1); break;
+                        case 2: setOrientations(Orientations.RIGHT); position.setX(position.getX() + 1); break;
                     }
                     break;
                 case LEFT:
                     switch(rand.nextInt(3)) {
-                        case 0: setOrientations(Orientations.UP); pos.setY(pos.getY() - 1); break;
-                        case 1: setOrientations(Orientations.DOWN); pos.setY(pos.getY() + 1); break;
-                        case 2: setOrientations(Orientations.RIGHT); pos.setX(pos.getX() + 1); break;
+                        case 0: setOrientations(Orientations.UP); position.setY(position.getY() - 1); break;
+                        case 1: setOrientations(Orientations.DOWN); position.setY(position.getY() + 1); break;
+                        case 2: setOrientations(Orientations.RIGHT); position.setX(position.getX() + 1); break;
                     }
                     break;
                 case RIGHT:
                     switch(rand.nextInt(3)) {
-                        case 0: setOrientations(Orientations.UP); pos.setY(pos.getY() - 1); break;
-                        case 1: setOrientations(Orientations.DOWN); pos.setY(pos.getY() + 1); break;
-                        case 2: setOrientations(Orientations.LEFT); pos.setX(pos.getX() - 1); break;
+                        case 0: setOrientations(Orientations.UP); position.setY(position.getY() - 1); break;
+                        case 1: setOrientations(Orientations.DOWN); position.setY(position.getY() + 1); break;
+                        case 2: setOrientations(Orientations.LEFT); position.setX(position.getX() - 1); break;
                     }
                     break;
             }
         }
-        lastpos = p;
-        return pos;
+        lastpos = position;
+        return position;
     }
 }
