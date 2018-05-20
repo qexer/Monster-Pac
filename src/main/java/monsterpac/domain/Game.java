1e57a02cb7dc38ab58871dd646d84e11924834f1
barmerc <36930010+barmerc@users.noreply.github.com>
2018-05-20 12:40:55 +0200

    add doc for Game and GameController

--- src/main/java/monsterpac/domain/Game.java
+++ src/main/java/monsterpac/domain/Game.java
@@ -1,9 +1,18 @@
 package monsterpac.domain;
 
+/**
+ * Felelőssége egy Table objektum vezérlése és felület biztosítása a játék végének
+ * és kimenetelének lekérdezéséhez.
+ */
 public class Game {
 
     private Table table;
 
+    /**
+     * Létrehoz egy Game példányt.
+     * @param table A vezérelt Table példány.
+     * @throws IllegalArgumentException Ha a table null.
+     */
     public Game(Table table) {
         if(table == null){
             throw new IllegalArgumentException("Table can not be null.");
@@ -11,16 +20,27 @@ public class Game {
         this.table = table;
     }
 
+    /**
+     * Végrehajt egy kört a játékból.
+     */
     public void act() {
         this.table.moveAll();
     }
 
+    /**
+     * Megadja, hogy a játékos nyert-e vagy sem.
+     * @return true ha a játékos nyert; egyébként false
+     */
     public boolean playerWon() {
         return !this.table.hasScoreLeft() && this.table.playerIsAlive(); //@TODO add Table.playerIsAlive() to UML diagram or implement it here
     }
 
     //@TODO remove playerLost() from UML diagram
 
+    /**
+     * Megadja, hogy a játék véget ért-e vagy sem.
+     * @return false ha a játék véget ért; egyébként true
+     */
     public boolean notEnded() { //@TODO rename in UML diagram
         if(playerWon()){
             return false;
--- src/main/java/monsterpac/domain/GameController.java
+++ src/main/java/monsterpac/domain/GameController.java
@@ -4,15 +4,26 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * A játék vezérléséért felel.
+ */
 public class GameController {
     private final Game game;
     private final ScheduledExecutorService ses;
 
+    /**
+     * Létrehoz egy GameController példányt.
+     * @param game A vezérelt játék.
+     */
     public GameController(Game game) {
         this.game = game;
         ses = Executors.newSingleThreadScheduledExecutor();
     }
 
+    /**
+     * Elindítja a játékot.
+     * @param afterGameAct minden játék kör után lefutó callback függvény
+     */
     public void start(Runnable afterGameAct) {
         ses.scheduleWithFixedDelay(new Runnable() {
             @Override
@@ -28,10 +39,19 @@ public class GameController {
         }, 0, 1, TimeUnit.SECONDS);
     }
 
+    /**
+     * Megállítja a játékot.
+     */
     public void stop() {
         ses.shutdownNow();
     }
 
+    /**
+     * Megadja a játék állását.
+     * @return "NO RESULT YET" ha még folyik a játék
+     *          "WIN" ha a játékos nyert
+     *          "LOSE" ha a jétkos vesztett
+     */
     public String getResult() {
         if(this.game.notEnded()){
             return "NO RESULT YET";
